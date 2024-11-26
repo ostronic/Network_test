a39b5f6f88a0025158d457c88e7b47c8cf3d34f5
fg_daemon <67126122+ostronic@users.noreply.github.com>
2024-11-25 20:19:05 -0500

    Add files via upload

--- network_func_check.py
+++ network_func_check.py
@@ -4,11 +4,12 @@ import sys, subprocess
 
 def Netfunc():
     ''' We are only checking for the return code, in which case if it is not 0(successful ping of count 10), we print a prompt status to the screen.'''
-    ostronics = subprocess.run(['ping', 'google.com', '-c', '10'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if not ostronics==0:
+    ostronics = subprocess.call(['ping', 'google.com', '-c', '10'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if ostronics != 0:
         print('CHECK YOUR NETWORK CONNECTION, VPN, OR CONNECT TO A WIFI NETWORK!!!')
         sys.exit(1)
-    return
+    elif ostronics == 0:
+        print('Networks all good :)')
 
 if __name__ == '__main__':
     Netfunc()
